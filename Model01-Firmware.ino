8c24b09f91b2564ea776a6398bc1a7002dc19284
Ted Reed <treed@purestorage.com>
2018-04-18 14:44:40 -0700

    swap warpend and printscreen

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -143,7 +143,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [KEYMAP_FUNCTION] =  KEYMAP_STACKED
   (Key_Backtick,      Key_F1,           Key_F2,        Key_F3,      Key_F4,        Key_F5,           Key_Insert,
-   Key_Tab,          Key_PrintScreen,   Key_mouseBtnL,     Key_mouseUp,    Key_mouseBtnR,    Key_mouseWarpEnd, Key_Home,
+   Key_Tab,          Key_mouseWarpEnd,  Key_mouseBtnL,     Key_mouseUp,    Key_mouseBtnR,    Key_PrintScreen, Key_Home,
    Key_mouseWarpNW,  Key_mouseWarpNE,   Key_mouseL,        Key_mouseDn,    Key_mouseR,       Key_mouseScrollUp,
    Key_mouseWarpSW,  Key_mouseWarpSE,   Key_mouseScrollL,  Key_mouseBtnM,  Key_mouseScrollR, Key_mouseScrollDn,  Key_End,
    ___, Key_Delete, ___, ___,
